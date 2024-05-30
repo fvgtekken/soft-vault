4db8bc5b4b4a3e9816eea423cb7071ca2413d8c2
federico <federicovongromann@gmail.com>
2024-05-30 17:16:25 -0300

    Add project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 1.5rem /* 24px */;
+`;
+
+
+export default function Home() {
+  return (
+    <Container>
+       <div>
+          Hello 1!
+      </div>
+       <div>
+          Hello 2!
+      </div>
+       <div>
+          Hello 2!
+      </div>
+    </Container>
+  );
+}
